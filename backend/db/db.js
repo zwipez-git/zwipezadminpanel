006b5bbb11101080d22822c1ff8fc805febfc772
abinraj207-max <abinraj207@gmail.com>
2026-04-28 16:33:58 +0530

    feat:testing

--- backend/db/db.js
+++ backend/db/db.js
@@ -14,14 +14,11 @@ let pool;
 if (process.env.DATABASE_URL) {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    // ssl: {
-    //   rejectUnauthorized: false,
-    // },
-    // // ssl: false,
-  ssl: process.env.NODE_ENV === "production"
-    ? { rejectUnauthorized: false }
-    : false
-});
+    ssl: {
+      rejectUnauthorized: false,
+    },
+    // ssl: false,
+  });
 } else {
   // Local DB config
   pool = new Pool({
@@ -30,15 +27,10 @@ if (process.env.DATABASE_URL) {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT || 5432,
-  //  ssl: {
-  //   rejectUnauthorized: false, 
-  // },
-  // // ssl: false,
-
-  ssl: process.env.NODE_ENV === "production"
-    ? { rejectUnauthorized: false }
-    : false
-
+   ssl: {
+    rejectUnauthorized: false, 
+  },
+  // ssl: false,
   });
 }
 
